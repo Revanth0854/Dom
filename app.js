3e6cd295d313f87b2057a1680f4b1145bdfe4d85
Revanth0854 <revnth0854@gmail.com>
2024-10-02 17:08:47 +0530

    popup completed

--- app.js
+++ app.js
@@ -45,10 +45,15 @@ let login = document.querySelector(".login");
 
 let mainForm = document.querySelector(".mainForm");
 
+let close = document.querySelector(".mainForm h2");
+
 login.addEventListener("click", () => {
-    mainForm.classList.toggle("newform")
+    mainForm.classList.add("newform")
 })
 
+close.addEventListener("click",()=>{
+    mainForm.classList.remove("newform");
+})
 
 // =============       NewsLetter      =============
 
@@ -60,8 +65,7 @@ let submit = document.querySelector(".submit");
 let space = document.querySelector(".read");
 let first = document.createElement("h3")
 let second = document.createElement("h3")
-submit.addEventListener("click", (e) => {
-    e.preventDefault()
+submit.addEventListener("click", () => {
     first.innerHTML = `${input1.value}`
     second.innerHTML = `${input2.value}`
     space.appendChild(first)
@@ -73,66 +77,26 @@ submit.addEventListener("click", (e) => {
 // ==================== image- pop-up   =================
 
 
-let img1 = document.querySelector(".img1");
-let img2 = document.querySelector(".img2");
-let img3 = document.querySelector(".img3");
-let img4 = document.querySelector(".img4");
-let img5 = document.querySelector(".img5");
-let img6 = document.querySelector(".img6");
 
-let pop1 = document.querySelector('.pop-up-image-1');
-let pop2 = document.querySelector('.pop-up-image-2');
-let pop3 = document.querySelector('.pop-up-image-3');
-let pop4 = document.querySelector('.pop-up-image-4');
-let pop5 = document.querySelector('.pop-up-image-5');
-let pop6 = document.querySelector('.pop-up-image-6');
+let images = document.querySelectorAll(".image img");
+let popup = document.querySelector(".popup");
+let img = document.createElement('img');
+let cross = document.querySelector(".popup h1");
+popup.appendChild(img);
+console.log(images)
 
 
-let closeBtn1 = document.querySelector('#close-pop1');
-let closeBtn2 = document.querySelector('#close-pop2');
-let closeBtn3 = document.querySelector('#close-pop3');
-let closeBtn4 = document.querySelector('#close-pop4');
-let closeBtn5 = document.querySelector('#close-pop5');
-let closeBtn6 = document.querySelector('#close-pop6');
-
-closeBtn1.addEventListener("click",()=>{
-    pop1.style.display = "none";
-})
-closeBtn2.addEventListener("click",()=>{
-    pop2.style.display = "none";
-})
-closeBtn3.addEventListener("click",()=>{
-    pop3.style.display = "none";
-})
-closeBtn4.addEventListener("click",()=>{
-    pop4.style.display = "none";
-})
-closeBtn5.addEventListener("click",()=>{
-    pop5.style.display = "none";
-})
-closeBtn6.addEventListener("click",()=>{
-    pop6.style.display = "none";
+images.forEach((item) => {
+    item.addEventListener("click", () => {
+        popup.style.display="block";
+        let attribute = item.getAttribute("src");
+        img.setAttribute("src", attribute);
+        console.log(attribute)
+    })
 })
 
-img1.addEventListener("click", () => {
-    pop1.style.display = 'block';
+cross.addEventListener("click",()=>{
+    popup.style.display="none";
 })
-img2.addEventListener("click", () => {
-    pop2.style.display = 'block'
-})
-img3.addEventListener("click", () => {
-    pop3.style.display = 'block'
 
-})
-img4.addEventListener("click", () => {
-    pop4.style.display = 'block'
 
-})
-img5.addEventListener("click", () => {
-    pop5.style.display = 'block'
-
-})
-img6.addEventListener("click", () => {
-    pop6.style.display = 'block'
-
-})
